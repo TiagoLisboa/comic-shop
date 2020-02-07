dd600e3fa60a5046c91f8a627efbddf479e2fb9c
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-02-06 21:34:37 -0300

    fetching comics

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../../services/api';
 
 export default function Home() {
-  return <div />;
+  const [comics, setComics] = useState([]);
+
+  useEffect(() => {
+    const fetchComics = async () => {
+      const response = await api.get('/comics');
+      const comics = response.data.data.results;
+
+      setComics(comics);
+    };
+    fetchComics();
+  }, []);
+
+  return comics.map(comic => (
+    <div key={comic.id}>
+      <img
+        src={
+          comic.thumbnail.path + '/portrait_xlarge.' + comic.thumbnail.extension
+        }
+      />
+      {comic.title}
+    </div>
+  ));
 }
